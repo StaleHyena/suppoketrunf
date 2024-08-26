6744fdc68be4f583b740ce6b7ef1ac146a1f5f3f
stale <stale@masba.net>
2024-08-26 10:51:37 -0300

    sunset the type rules idea

--- pokecards.c
+++ pokecards.c
@@ -28,12 +28,10 @@ char *pokecard_repr_simplestr_salloc_aligned(uint16_t uid, size_t namelen, size_
       " HP %3d"
       " ATK %3d"
       " DEF %3d"
-      " SP. ATK %3d"
-      " SP. DEF %3d"
-      " GROUP %c"
+      " SPATK %3d"
+      " SPDEF %3d"
       "%s",
       s.hp, s.attack, s.defense, s.sp_attack, s.sp_defense,
-      card_group_char(pokecard_group_of(uid)),
       s.legendary? " ! LEGENDARY !" : ""
     );
   return buf;
@@ -44,42 +42,3 @@ char *pokecard_repr_simplestr_salloc(uint16_t uid) {
   return pokecard_repr_simplestr_salloc_aligned(uid, 0, 0);
 }
 
-card_group_t
-pokecard_group_of(uint16_t uid) {
-  int first = pokemon_types[uid][0];
-
-  // These only make sense for the 1st gen, FIXME
-#define IS(TYPE) (first == TYPE)
-    if (0 // 42 pokes
-      || IS(Normal)
-      || IS(Fairy)
-      || IS(Dragon)
-      || IS(Ice)
-      || IS(Psychic)
-    ) return GROUP_A;
-    else if (0 // 41 pokes
-      || IS(Water)
-      || IS(Rock)
-    ) return GROUP_B;
-    else if (0 // 41 pokes
-      || IS(Fire)
-      || IS(Electric)
-      || IS(Ghost)
-      || IS(Bug)
-    ) return GROUP_C;
-    else if (0 // 42 pokes
-      || IS(Grass)
-      || IS(Fighting)
-      || IS(Poison)
-      || IS(Ground)
-    ) return GROUP_D;
-#undef IS
-
-  return GROUP_UNDEF;
-}
-
-char
-card_group_char(card_group_t cg) {
-  char *LUT = "?ABCD";
-  return LUT[cg];
-}
--- pokecards.h
+++ pokecards.h
@@ -6,18 +6,7 @@
 
 typedef uint16_t pokecard_uid;
 
-typedef enum {
-  GROUP_UNDEF = 0,
-  GROUP_A = 1,
-  GROUP_B = 2,
-  GROUP_C = 3,
-  GROUP_D = 4,
-} card_group_t;
-
-char card_group_char(card_group_t cg);
-
 char *pokecard_repr_simplestr_salloc(uint16_t uid);
 char *pokecard_repr_simplestr_salloc_aligned(uint16_t uid, size_t namelen, size_t grace);
-card_group_t pokecard_group_of(uint16_t uid);
 
 #endif // POKECARDS_H__
